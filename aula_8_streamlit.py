b4fd10d62aa9ec239a3056cb78229836a091ee35
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-09-10 13:42:54 -0300

    Update aula_8_streamlit.py

--- aula_8_streamlit.py
+++ aula_8_streamlit.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
-st.write("Hello ,let's learn how to build a streamlit app together")
+#imprimindo uma mensagem na tela
 st.write("Vamos aprender streamlit juntos!")
 
+#usando várias formas de digitar
 st.title("Este é o título do app")
 st.header("Este é o subtítulo")
 st.subheader("Este é o terceiro subtítulo")
@@ -11,11 +12,12 @@ st.caption("Esta é a a legenda")
 st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
+#criando elementos gráficos
 st.checkbox('Sim')
 st.button('Clique')
 st.radio('Selecione seu gênero',['Masculino','Feminino'])
 st.selectbox('Selecione seu gênero',['Masculino','Feminino'])
-st.multiselect('Escolha um planeta',['Jupiter', 'Marte', 'Netuno'])
+st.multiselect('Escolha um departamento',['DCS', 'DE', 'DIR'])
 st.select_slider('Selecione uma resposta', ['Ruim', 'Bom', 'Excelente'])
 st.slider('Selecione um número', 0,50)
 
