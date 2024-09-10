4140d1d97a282ba62c2d2f5acdb028f648234fac
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-09-10 09:53:31 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv('/content/WHO_time_series.csv')
+df = pd.read_csv('WHO_time_series.csv')
 
 st.set_page_config(
     page_title="DashCovid",
