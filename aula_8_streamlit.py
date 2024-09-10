63f98fccce17f2a95e4ca8aa06b7923d5df146d7
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-09-10 10:53:40 -0300

    Update aula_8_streamlit.py

--- aula_8_streamlit.py
+++ aula_8_streamlit.py
@@ -35,12 +35,15 @@ st.text_area('Description')
 st.file_uploader('Upload a photo')
 st.color_picker('Choose your favorite color')
 
+import time
+
 st.balloons()
 st.progress(10)
-with st.spinner('Wait for it...'):    
+with st.spinner('Wait for it...'):
     time.sleep(10)
 
-st.success("You did it !")st.error("Error")
-st.warnig("Warning")
+st.success("You did it !")
+st.error("Error")
+st.warning("Warning")
 st.info("It's easy to build a streamlit app")
 st.exception(RuntimeError("RuntimeError exception"))
