e464dddd7fe7ac08ff18494e663c67bbcc90fa67
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-09-11 17:04:02 -0300

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -1,6 +1,17 @@
 import streamlit as st
 import pandas as pd
 
+df = pd.DataFrame({
+    'Nome do servidor': ['Adriana', 'Monica', 'Samara'],
+    'Salario': [1200,300,5000]
+})
+
+st.write("Criando uma tabela!")
+
+#tabelas interativas
+st.write(df) 
+
+#inserindo um selectbox
 option = st.selectbox(
     'Qual servidor você gostaria de selecionar?',
      df['Nome do servidor'])
