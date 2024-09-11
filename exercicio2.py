8bdebc571ecd4063f5d6a239751d0285d1b84444
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-09-11 17:02:49 -0300

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -1,13 +1,8 @@
-#Usando tabelas estáticas
 import streamlit as st
 import pandas as pd
 
-df = pd.DataFrame({
-    'Nome do servidor': ['Adriana', 'Monica', 'Samara'],
-    'Salario': [1200,300,5000]
-})
+option = st.selectbox(
+    'Qual servidor você gostaria de selecionar?',
+     df['Nome do servidor'])
 
-st.write("Criando uma tabela!")
-
-#tabelas interativas
-st.table(df) 
+'You selected: ', option
