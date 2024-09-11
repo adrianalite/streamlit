5f9750ce58d932e12876caa017a5966a3c8ffe71
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-09-11 17:01:02 -0300

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -2,6 +2,11 @@
 import streamlit as st
 import pandas as pd
 
+df = pd.DataFrame({
+    'Nome do servidor': ['Adriana', 'Monica', 'Samara'],
+    'Salario': [1200,300,5000]
+})
+
 st.write("Criando uma tabela!")
 
 #tabelas interativas
