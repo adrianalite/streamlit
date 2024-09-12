43b1bf2bb4810c87f3a2db0951030e5251703d19
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-09-11 21:07:12 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+st.title('Localização das comunidades quilombolas')
+
 df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv', sep=';')
 df.head()
 
