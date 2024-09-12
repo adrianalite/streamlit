e6017f97c6183d6560a5ef7cadc5aa3dbfbd5d50
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-09-11 21:15:04 -0300

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -1,7 +1,8 @@
+#inserindo o título
 import streamlit as st
 import pandas as pd
 
-st.title('Localização das comunidades quilombolas (2022))
+st.title('Localização das comunidades quilombolas (2022)')
 
 df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv', sep=';')
 df.head()
