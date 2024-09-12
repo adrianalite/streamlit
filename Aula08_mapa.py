07ec2c81c0978ceb18dd188e4a08301813880bfb
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-09-12 11:55:32 -0300

    Update Aula08_mapa.py

--- Aula08_mapa.py
+++ Aula08_mapa.py
@@ -2,10 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title('Localização das comunidades quilombolas (2022)')
-
-df = pd.read_csv('https://github.com/adrianalite/datasets/blob/main/BR_LQs_CD2022.csv')
-
-#fillna
+df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
 df.fillna(0, inplace=True)
 list = ['Lat_d', 'Long_d']
 df[list] = df[list].apply(pd.to_numeric, errors='coerce')
@@ -14,11 +11,7 @@ estados = df['NM_UF'].unique()
 estadoFiltro = st.selectbox(
     'Qual estado selecionar?',
      estados)
-
 dadosFiltrados = df[df['NM_UF'] == estadoFiltro]
-
-#Mostrar tabela
 if st.checkbox('Mostrar tabela'):
   st.write(dadosFiltrados)
-    
 st.map(dadosFiltrados)
