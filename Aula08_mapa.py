d0184cd58718eb18fd314420cb2583ba41d480f1
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-09-12 12:00:12 -0300

    Update Aula08_mapa.py

--- Aula08_mapa.py
+++ Aula08_mapa.py
@@ -4,6 +4,7 @@ import pandas as pd
 st.title('Localização das comunidades quilombolas (2022)')
 df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
 df.fillna(0, inplace=True)
+df.drop(columns=['Unnamed: 0'], inplace=True)
 list = ['Lat_d', 'Long_d']
 df[list] = df[list].apply(pd.to_numeric, errors='coerce')
 df.rename(columns={'Lat_d': 'LATITUDE', 'Long_d':'LONGITUDE'}, inplace=True)
