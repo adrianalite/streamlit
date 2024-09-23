ee4b9990a26f2d1c2363f739baef5097601d5989
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-09-23 10:34:48 -0300

    Update Aula08_slider.py

--- Aula08_slider.py
+++ Aula08_slider.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 st.title('Localização das comunidades quilombolas (2022)')
 
-#preparando o dataframe
 df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
-
-#criando um slider
+df.info()
 numero = st.slider('Selecione um número de linhas a serem exibidas', min_value = 0, max_value = 100)
 st.write(df.head(numero))
+df['NM_UF'].value_counts()
+st.bar_chart(df['NM_UF'].value_counts())
