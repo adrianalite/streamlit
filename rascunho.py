c1af2608efe0f7399dcde632adedab3282436421
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-09-23 14:04:32 -0300

    Update rascunho.py

--- rascunho.py
+++ rascunho.py
@@ -1,3 +1,24 @@
+#programa apresentado durante a aula
+import streamlit as st
+import pandas as pd
+
+st.title('Localização das comunidades quilombolas (2022)')
+df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
+
+#apresentar o dataframe
+#https://www.ibge.gov.br/geociencias/organizacao-do-territorio/estrutura-territorial/27385-localidades.html?=&t=downloads
+
+df.info()
+numero = st.slider('Selecione um número de linhas a serem exibidas', min_value = 0, max_value = 100)
+st.write(df.head(numero))
+
+#quantidade de comunidades por estado
+df['NM_UF'].value_counts()
+
+#número de comunidades por estado
+st.header('Número de comunidades por UF')
+st.bar_chart(df['NM_UF'].value_counts())
+
 #sugestões de incrementos para a questão 6
 import streamlit as st;
 import pandas as pd
