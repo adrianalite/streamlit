1505c1387f9691c9ca3a1103c9b78db2a661655a
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-09-23 10:09:16 -0300

    Update Aula08_slider.py

--- Aula08_slider.py
+++ Aula08_slider.py
@@ -1,9 +1,11 @@
-#criando um slider
 import streamlit as st
 import pandas as pd
+
+st.title('Localização das comunidades quilombolas (2022)')
 
 #preparando o dataframe
 df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
 
+#criando um slider
 numero = st.slider('Selecione um número de linhas a serem exibidas', min_value = 0, max_value = 100)
 st.write(df.head(numero))
