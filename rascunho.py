fda390c83d5957ac18755e0a1ba131ce19ff7312
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-09-23 13:40:34 -0300

    Update rascunho.py

--- rascunho.py
+++ rascunho.py
@@ -3,18 +3,14 @@ import streamlit as st;
 import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
-display(df)
+st.write(df)
 
-#informando dados sobre estatística descritiva
-
-#incluindo a coluna região
+#dados sobre estatística descritiva
 qtdeMunicipios = df['NM_MUNIC'].unique()
-
-st.write("A quantidade de municípios com localização quilombola é " + qtdeMunicipios);
-st.write();
+st.write("A quantidade de municípios com localização quilombola é " + qtdeMunicipios)
 
 qtdeComunidades = df['NM_AGLOM'].unique()
-st.write("A quantidade de comunidades quilombolas é " + str(qtdeComunidades));
+st.write("A quantidade de comunidades quilombolas é " + str(qtdeComunidades))
 
 #os dez municípios com mais comunidades
 st.header('Os dez municípios com mais comunidades quilombolas')
