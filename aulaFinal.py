ec26ec74e327c2d68f4c323da14db18302d9331f
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-10-05 14:49:26 -0300

    Update aulaFinal.py

--- aulaFinal.py
+++ aulaFinal.py
@@ -20,16 +20,34 @@ dfHomens['sexo'] = 'M'
 #unindo os dataframes
 df = pd.concat([dfMulheres, dfHomens])
 
+#ocorrencias totais
 ocorrencias = df['siglaUf'].value_counts()
-
 dfEstados = pd.DataFrame({
     'siglaUf': ocorrencias.index,
     'quantidade': ocorrencias.values}
     )
 
+#ocorrências sobre as mulheres
+ocorrenciasMulheres = dfMulheres['siglaUf'].value_counts()
+dfEstadosMulheres = pd.DataFrame({
+    'siglaUf': ocorrenciasMulheres.index,
+    'quantidade': ocorrenciasMulheres.values}
+    )
+
+#ocorrências sobre os homens
+ocorrenciasHomens = dfHomens['siglaUf'].value_counts()
+dfEstadosHomens = pd.DataFrame({
+    'siglaUf': ocorrenciasHomens.index,
+    'quantidade': ocorrenciasHomens.values}
+    )
+
 st.title('Dados sobre os deputados')
 st.dataframe(df)
-st.write('Total de deputados por estado')
+st.write('Total de deputadas mulheres por estado')
+st.bar_chart(dfEstadosMulheres, x = 'siglaUf', y = 'quantidade', x_label='Siglas dos estados', y_label='Quantidade de deputados')
+
+st.write('Total de deputados homens por estado')
+st.bar_chart(dfEstadosHomens, x = 'siglaUf', y = 'quantidade', x_label='Siglas dos estados', y_label='Quantidade de deputados')
+
+st.write('Total de deputados estado')
 st.bar_chart(dfEstados, x = 'siglaUf', y = 'quantidade', x_label='Siglas dos estados', y_label='Quantidade de deputados')
-
-
