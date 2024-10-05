084e93c9c60d844f2eb31f5b949363b7f067e53f
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-10-05 12:30:24 -0300

    Update aulaFinal.py

--- aulaFinal.py
+++ aulaFinal.py
@@ -1,4 +1,19 @@
-#Gráficos treemap usando streamlit
-import streamlit as st
+import requests as rq
+
+#carregando o dataframe de mulheres
+url = 'https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=F&ordem=ASC&ordenarPor=nome'
+resposta = rq.get(url)
+dadosJSON = resposta.json()
+dfMulheres = pd.DataFrame(dadosJSON['dados'])
+
+#carregando o dataframe de mulheres
+url = 'https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=M&ordem=ASC&ordenarPor=nome'
+resposta = rq.get(url)
+dadosJSON = resposta.json()
+dfHomens = pd.DataFrame(dadosJSON['dados'])
 
 st.bar_chart(dfMulheres['siglaPartido'].value_counts())
+
+
+
+
