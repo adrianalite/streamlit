b70e2afeb3f8f7f71c3dc00f8b8e6bea203781df
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-10-11 17:49:59 -0300

    Update estudoCasoQuilombolas.py

--- estudoCasoQuilombolas.py
+++ estudoCasoQuilombolas.py
@@ -1,14 +1,9 @@
 #versao completa com colunas
 import streamlit as st
 import pandas as pd
-import numpy as np
 
 st.title('Localização das comunidades quilombolas (2022)')
 df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
-
-# Substitui valores 'None' por np.nan
-df['Lat_d'] = df['Lat_d'].replace([None], np.nan)
-df['Long_d'] = df['Long_d'].replace([None], np.nan)
 
 #limpando os dados
 df.fillna(0, inplace=True)
@@ -17,7 +12,6 @@ list = ['Lat_d', 'Long_d']
 #convertendo para numeros
 df[list] = df[list].apply(pd.to_numeric, errors='coerce')
 #df.rename(columns={'Lat_d': 'LATITUDE', 'Long_d':'LONGITUDE'}, inplace=True)
-
 
 estados = df['NM_UF'].unique()
 estadoFiltro = st.selectbox(
