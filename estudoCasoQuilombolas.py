67bf4447c7442d881e1a34b950245033540c67ad
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-10-11 17:29:02 -0300

    Update estudoCasoQuilombolas.py

--- estudoCasoQuilombolas.py
+++ estudoCasoQuilombolas.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 st.title('Localização das comunidades quilombolas (2022)')
-df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/refs/heads/main/dados_limpos_IBGE.csv')
 
 # Substitui valores 'None' por np.nan
 df['Lat_d'] = df['Lat_d'].replace([None], np.nan)
