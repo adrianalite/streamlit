5bafe1d4ab8130334b596e3fed2a6de8cc743531
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-11-18 08:53:50 -0300

    Update aulaFinal.py

--- aulaFinal.py
+++ aulaFinal.py
@@ -2,53 +2,57 @@ import requests as rq
 import pandas as pd
 import streamlit as st
 
-#identificando as mulheres
+#item 3
+#substituir {sexo} por F ou M na URL abaixo
+#url = 'https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo={sexo}&ordem=ASC&ordenarPor=nome'
 
+
+#item 4
+#identificando as mulheres
 url = 'https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=F&ordem=ASC&ordenarPor=nome'
 resposta = rq.get(url)
 dadosJSON = resposta.json()
 dfMulheres = pd.DataFrame(dadosJSON['dados'])
 dfMulheres['sexo'] = 'F'
-
 #identificando os homens
 url = 'https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=M&ordem=ASC&ordenarPor=nome'
 resposta = rq.get(url)
 dadosJSON = resposta.json()
 dfHomens = pd.DataFrame(dadosJSON['dados'])
 dfHomens['sexo'] = 'M'
-
 #unindo os dataframes
 df = pd.concat([dfMulheres, dfHomens])
 
+#item 5
 #Filtrando df por sexo
 #inserindo um selectbox
 opcao = st.selectbox(
     'Qual o sexo?',
      df['sexo'].unique())
-
 dfFiltrado = df[df['sexo'] == opcao]
 st.title('Deputados do sexo ' + opcao)
 
+#item 6
 #ocorrencias totais
+#procurando no chat GPT: Como calcular a 
+#quantidade de deputados por estado?
 ocorrencias = dfFiltrado['siglaUf'].value_counts()
 dfEstados = pd.DataFrame({
     'siglaUf': ocorrencias.index,
     'quantidade': ocorrencias.values}
     )
 
-#total de deputados
-total = df['id'].count()
-st.metric('Total de deputados', total)
-
-#Porcentagem de homens
+#item 7
+#total de homens
 totalHomens = dfHomens['id'].count()
-st.metric('Porcentagem de Homens', totalHomens/total * 100)
-
+st.metric('Total de Homens', totalHomens)
 #total de mulheres
 totalMulheres = dfMulheres['id'].count()
-st.metric('Porcentagem de Mulheres', totalMulheres/total * 100)
-
+st.metric('Total de Mulheres', totalMulheres)
 st.write('Total de deputadas do sexo ' + opcao)
-st.bar_chart(dfEstados, x = 'siglaUf', y = 'quantidade', x_label='Siglas dos estados', y_label='Quantidade de deputados')
-
+st.bar_chart(dfEstados, 
+             x = 'siglaUf', 
+             y = 'quantidade', 
+             x_label='Siglas dos estados', 
+             y_label='Quantidade de deputados')
 st.dataframe(dfFiltrado)
